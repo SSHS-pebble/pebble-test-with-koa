bfd1a4d05785e13970d996334d6bcc813c764779
Sungbin Jo <pcr910303@icloud.com>
2018-10-19 11:29:42 +0900

    Change DB insert for student code and phone numbers

--- app/route/signup.js
+++ app/route/signup.js
@@ -1,13 +1,15 @@
 const bcrypt = require("bcrypt");
 
 module.exports = async (ctx, next) => {
-    if(!ctx.request.body.email || !ctx.request.body.name || !ctx.request.body.password) {
+    if(!ctx.request.body.email || !ctx.request.body.name || !ctx.request.body.code || !ctx.request.body.password) {
         ctx.throw(400);
     }
     try {
         await ctx.userCollection.insertOne({
             email: ctx.request.body.email,
             name: ctx.request.body.name,
+            code: ctx.request.body.code,
+            phone: ctx.request.body.phone,
             password: await bcrypt.hash(ctx.request.body.password, 10)
         });
     } catch(e) {
