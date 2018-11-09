8a6aae57278ad842fa16a327db89879ebfd0354e
Sungbin Jo <pcr910303@icloud.com>
2018-11-09 12:45:20 +0900

    Revert "Dummy view, only login working!"
    This reverts commit 80d30dc7433c39209661d03cc9b549debf2fe38e.

--- app/view/src/index.jsx
+++ app/view/src/index.jsx
@@ -1,7 +1,5 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
-require("typeface-roboto");
 
-const App = require("./app.jsx");
 const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+ReactDOM.render(<p>Hello, pebble!</p>, root);
