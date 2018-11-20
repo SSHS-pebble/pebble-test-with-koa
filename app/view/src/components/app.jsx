4cae43cf4367e72e6bf9280da7c1882f603899a6
Sungbin Jo <pcr910303@icloud.com>
2018-11-21 08:53:01 +0900

    Use react-router for routing

--- app/view/src/components/app.jsx
+++ app/view/src/components/app.jsx
@@ -1,16 +1,19 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 const { CssBaseline } = require("@material-ui/core");
+const { BrowserRouter } = require("react-router-dom");
 const { Provider } = require("react-redux");
 
 const Main = require("./main.jsx");
 const store = require("../redux/store.js");
 
 module.exports = props => (
-    <div>
+    <React.Fragment>
       <CssBaseline />
-      <Provider store={store}>
-        <Main />
-      </Provider>
-    </div>
+      <BrowserRouter>
+        <Provider store={store}>
+          <Main />
+        </Provider>
+      </BrowserRouter>
+    </React.Fragment>
 );
--- app/view/src/components/drawer.jsx
+++ app/view/src/components/drawer.jsx
@@ -14,6 +14,7 @@ const {
     ExpandMore: ExpandMoreIcon,
     School: SchoolIcon
 } = require("@material-ui/icons");
+const { Link } = require("react-router-dom");
 const { connect } = require("react-redux");
 
 const actions = require("../redux/actions.js");
@@ -27,58 +28,44 @@ const styles = theme => ({
     }
 });
 
+const DrawerSublist = props => (
+    <React.Fragment>
+      <ListItem button onClick={props.onToggle}>
+        <ListItemIcon>
+          {props.icon}
+        </ListItemIcon>
+        <ListItemText inset>{props.title}</ListItemText>
+        {props.isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+      </ListItem>
+      <Collapse in={props.isOpen}>
+        <List>
+          {props.subItem.map(item => props.children(item))}
+        </List>
+      </Collapse>
+    </React.Fragment>
+);
+
 module.exports = withStyles(styles)(connect(state => state, actions)(props => (
     <Drawer open={props.drawer.isDrawerOpen} onClose={props.toggleDrawer}>
       <List component="nav" className={props.classes.drawer}>
-        <ListItem button onClick={e => props.toggleDrawerItem("Notice")}>
-          <ListItemIcon>
-            <SchoolIcon />
-          </ListItemIcon>
-          <ListItemText inset>공지사항</ListItemText>
-          {props.drawer.isNoticeOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-        </ListItem>
-        <Collapse in={props.drawer.isNoticeOpen}>
-          <List>
-            <ListItem button className={props.classes.nestedListItem}>
-              <ListItemText>연구</ListItemText>
-            </ListItem>
-            <ListItem button className={props.classes.nestedListItem}>
-              <ListItemText>행사</ListItemText>
-            </ListItem>
-            <ListItem button className={props.classes.nestedListItem}>
-              <ListItemText>학사일정</ListItemText>
-            </ListItem>
-          </List>
-        </Collapse>
-        <ListItem button onClick={e => props.toggleDrawerItem("MoveSeat")}>
-          <ListItemIcon>
-            <MoveSeatIcon />
-          </ListItemIcon>
-          <ListItemText inset>이석</ListItemText>
-          {props.drawer.isMoveSeatOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-        </ListItem>
-        <Collapse in={props.drawer.isMoveSeatOpen}>
-          <List>
-            <ListItem button className={props.classes.nestedListItem}>
-              <ListItemText>1학년 공강실</ListItemText>
-            </ListItem>
-            <ListItem button className={props.classes.nestedListItem}>
-              <ListItemText>2학년 공강실</ListItemText>
-            </ListItem>
-            <ListItem button className={props.classes.nestedListItem}>
-              <ListItemText>3학년 공강실</ListItemText>
-            </ListItem>
-            <ListItem button className={props.classes.nestedListItem}>
-              <ListItemText>세미나실</ListItemText>
-            </ListItem>
-            <ListItem button className={props.classes.nestedListItem}>
-              <ListItemText>단체 이석</ListItemText>
-            </ListItem>
-            <ListItem button className={props.classes.nestedListItem}>
-              <ListItemText>이석 명단 확인</ListItemText>
-            </ListItem>
-          </List>
-        </Collapse>
+        <DrawerSublist onToggle={e => props.toggleDrawerItem("Notice")} title="공지사항" icon={<SchoolIcon />} subItem={[{ name: "연구", to: "/notice/research" },{ name: "행사", to: "/notice/event" }, { name: "학사일정", to: "/notice/calendar"}]} isOpen={props.drawer.isNoticeOpen}>
+          {item => (
+              <li>
+                <ListItem button className={props.classes.nestedListItem} component={Link} to={item.to}>
+                  <ListItemText>{item.name}</ListItemText>
+                </ListItem>
+              </li>
+          )}
+        </DrawerSublist>
+        <DrawerSublist onToggle={e => props.toggleDrawerItem("MoveSeat")} title="이석" icon={<MoveSeatIcon />} subItem={[{ name: "1학년 공강실", to: "/move-seat/grade/1" }, { name: "2학년 공강실", to: "/move-seat/grade/2" }, { name: "3학년 공강실", to: "/move-seat/grade/3" }, { name: "세미나실", to: "/move-seat/seminar" }, { name: "단체 이석", to: "/move-seat/group" }, { name: "이석 명단 확인", to: "/move-seat/list" }]} isOpen={props.drawer.isMoveSeatOpen}>
+          {item => (
+              <li>
+                <ListItem button className={props.classes.nestedListItem} component={Link} to={item.to}>
+                  <ListItemText>{item.name}</ListItemText>
+                </ListItem>
+              </li>
+          )}
+        </DrawerSublist>
       </List>
     </Drawer>
 )));
