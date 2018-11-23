e7761051595f384733d6a6e51979931fcfca8045
Sungbin Jo <pcr910303@icloud.com>
2018-11-23 14:14:26 +0900

    Make mui use next typography

--- app/view/src/components/app.jsx
+++ app/view/src/components/app.jsx
@@ -1,18 +1,30 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
-const { CssBaseline } = require("@material-ui/core");
+const {
+    CssBaseline,
+    MuiThemeProvider
+} = require("@material-ui/core");
+const { createMuiTheme } = require("@material-ui/core/styles");
 const { BrowserRouter } = require("react-router-dom");
 const { Provider } = require("react-redux");
 
 const Main = require("./main.jsx");
 const store = require("../redux/store.js");
 
+const theme = createMuiTheme({
+    typography: {
+        useNextVariants: true,
+    }
+});
+
 module.exports = props => (
     <React.Fragment>
-      <CssBaseline />
       <BrowserRouter>
         <Provider store={store}>
-          <Main />
+          <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <Main />
+          </MuiThemeProvider>
         </Provider>
       </BrowserRouter>
     </React.Fragment>
