2d283230b6cce85f1bcf49b663b4b4049d0c59e1
Sungbin Jo <pcr910303@icloud.com>
2018-11-26 10:24:15 +0900

    Better abstraction on making drawer

--- app/view/src/modules/main/components/drawer.jsx
+++ app/view/src/modules/main/components/drawer.jsx
@@ -29,52 +29,70 @@ const styles = theme => ({
     }
 });
 
-const DrawerSublist = props => (
-    <React.Fragment>
-      <ListItem button onClick={props.onToggle}>
-        <ListItemIcon>
-          {props.icon}
-        </ListItemIcon>
-        <ListItemText inset>{props.title}</ListItemText>
-        {props.isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+const ListItemButton = ({ children, ...props }) => (
+    <li>
+      <ListItem button {...props}>
+        {children}
       </ListItem>
-      <Collapse in={props.isOpen}>
-        <List>
-          {props.subItem.map(item => props.children(item))}
-        </List>
+    </li>
+);
+
+const CollapsableListItem = ({ children, header, open, onClick, ...props }) => (
+    <React.Fragment>
+      <ListItemButton onClick={onClick} {...props}>
+        {header}
+      </ListItemButton>
+      <Collapse component="li" in={open}>
+        {children}
       </Collapse>
     </React.Fragment>
 );
 
+const ExpandIcon = ({ expandable }) => expandable ? <ExpandMoreIcon /> : <ExpandLessIcon />;
+
 module.exports = withStyles(styles)(connect(state => ({ main: state.main }), actions)(props => (
     <Drawer open={props.main.isDrawerOpen} onClose={props.toggleDrawer}>
       <List component="nav" className={props.classes.drawer}>
-        <li>
-          <ListItem button component={Link} to="/meal">
-            <ListItemIcon>
-              <MealIcon />
-            </ListItemIcon>
-            <ListItemText>학교 급식</ListItemText>
-          </ListItem>
-        </li>
-        <DrawerSublist onToggle={e => props.toggleDrawerItem("Notice")} title="공지사항" icon={<SchoolIcon />} subItem={[{ name: "연구", to: "/notice/research" },{ name: "행사", to: "/notice/event" }, { name: "학사일정", to: "/notice/calendar"}]} isOpen={props.main.isNoticeOpen}>
-          {item => (
+        <ListItemButton>
+          <ListItemIcon>
+            <MealIcon />
+          </ListItemIcon>
+          <ListItemText>학교 급식</ListItemText>
+        </ListItemButton>
+        <CollapsableListItem open={props.main.isNoticeOpen} onClick={e => props.toggleDrawerItem("Notice")} header={
+            <React.Fragment>
+              <ListItemIcon>
+                <SchoolIcon />
+              </ListItemIcon>
+              <ListItemText>공지사항</ListItemText>
+              <ExpandIcon expandable={!props.main.isNoticeOpen}/>
+            </React.Fragment>
+        }>
+          {[{ name: "연구", to: "/notice/research" },{ name: "행사", to: "/notice/event" }, { name: "학사일정", to: "/notice/calendar"}].map(({ name, to }) => (
               <li>
-                <ListItem button className={props.classes.nestedListItem} component={Link} to={item.to}>
-                  <ListItemText>{item.name}</ListItemText>
+                <ListItem button className={props.classes.nestedListItem} component={Link} to={to}>
+                  <ListItemText>{name}</ListItemText>
                 </ListItem>
               </li>
-          )}
-        </DrawerSublist>
-        <DrawerSublist onToggle={e => props.toggleDrawerItem("MoveSeat")} title="이석" icon={<MoveSeatIcon />} subItem={[{ name: "1학년 공강실", to: "/move-seat/grade/1" }, { name: "2학년 공강실", to: "/move-seat/grade/2" }, { name: "3학년 공강실", to: "/move-seat/grade/3" }, { name: "세미나실", to: "/move-seat/seminar" }, { name: "단체 이석", to: "/move-seat/group" }, { name: "이석 명단 확인", to: "/move-seat/list" }]} isOpen={props.main.isMoveSeatOpen}>
-          {item => (
+          ))}
+        </CollapsableListItem>
+        <CollapsableListItem open={props.main.isMoveSeatOpen} onClick={e => props.toggleDrawerItem("MoveSeat")} header={
+            <React.Fragment>
+              <ListItemIcon>
+                <MoveSeatIcon />
+              </ListItemIcon>
+              <ListItemText>이석</ListItemText>
+              <ExpandIcon expandable={!props.main.isNoticeOpen}/>
+            </React.Fragment>
+        }>
+          {[{ name: "1학년 공강실", to: "/move-seat/grade/1" }, { name: "2학년 공강실", to: "/move-seat/grade/2" }, { name: "3학년 공강실", to: "/move-seat/grade/3" }, { name: "세미나실", to: "/move-seat/seminar" }, { name: "단체 이석", to: "/move-seat/group" }, { name: "이석 명단 확인", to: "/move-seat/list" }].map(({ name, to }) => (
               <li>
-                <ListItem button className={props.classes.nestedListItem} component={Link} to={item.to}>
-                  <ListItemText>{item.name}</ListItemText>
+                <ListItem button className={props.classes.nestedListItem} component={Link} to={to}>
+                  <ListItemText>{name}</ListItemText>
                 </ListItem>
               </li>
-          )}
-        </DrawerSublist>
+          ))}
+        </CollapsableListItem>
       </List>
     </Drawer>
 )));
