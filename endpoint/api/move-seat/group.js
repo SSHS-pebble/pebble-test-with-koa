f65704c329d4870812dc115bfdcda88c82d97e81
chosanglyul <chosanglyul@gmail.com>
2020-04-05 11:15:57 +0900

    Add operation add user in group moveseat

--- endpoint/api/move-seat/group.js
+++ endpoint/api/move-seat/group.js
@@ -4,9 +4,7 @@ const isNumber = require('is-number');
 
 module.exports = {
     post: async (ctx, next) => {
-        if(!isNumber(ctx.params.code) || ctx.params.code <= 0) ctx.throw(400);
         const classCode = parseInt(ctx.params.code, 10);
-
         const classInfo = await ctx.state.collection.classes.findOne({ code: classCode });
         if(((1 << (ctx.state.user.grade-1)) & classInfo.moveseat.group) == 0) ctx.throw(400);
         const count = await ctx.state.collection.moveSeatState.countDocuments({ classcode: classCode });
@@ -14,7 +12,7 @@ module.exports = {
         
         const users = JSON.parse(ctx.request.body.users);
         if(!Array.isArray(users)) ctx.throw(400);
-        if(users.length+1 > classInfo.moveseat.limit) ctx.throw(400);
+        if(users.length == 0 || users.length+1 > classInfo.moveseat.limit) ctx.throw(400);
 
         const userinfo = await Promise.all(
             users.map(async (user) => {
@@ -23,7 +21,6 @@ module.exports = {
                 if(!ithuser) ctx.throw(403);
                 ithuser.password = undefined;
                 if(((1 << (ithuser.grade-1)) & classInfo.moveseat.group) == 0) ctx.throw(400);
-                console.log(ithuser);
                 return ithuser;
             })
         );
@@ -40,7 +37,7 @@ module.exports = {
         await next();
     },
     get: async (ctx, next) => {
-        ctx.body.data = await ctx.state.collection.moveSeatState.find().toArray();
+        ctx.body.data = await ctx.state.collection.moveSeatState.find({ classcode: parseInt(ctx.params.code, 10) }).toArray();
         await next();
     },
     delete: async (ctx, next) => {
@@ -48,16 +45,33 @@ module.exports = {
         await next();
     },
     put: async (ctx, next) => {
-        if(!isNumber(ctx.request.body.deleteUser)) ctx.throw(400);
-        const moveSeatInfo = await ctx.state.collection.moveSeatState.findOne({ 'primary.email': ctx.state.user.email});
-        const deleteIndex = moveSeatInfo.secondary.findIndex(element => (element.code == parseInt(ctx.request.body.deleteUser)));
-        if(deleteIndex == -1) ctx.throw(400);
-        moveSeatInfo.secondary.splice(deleteIndex, 1);
+        if(!isNumber(ctx.request.body.userInfo) || !isNumber(ctx.request.body.operation)) ctx.throw(400);
+        const moveSeatInfo = await ctx.state.collection.moveSeatState.findOne({ 'primary.email': ctx.state.user.email });
+        if(!moveSeatInfo) ctx.throw(400);
+        const userIndex = moveSeatInfo.secondary.findIndex(element => (element.code == parseInt(ctx.request.body.userInfo, 10)));
+
+        if(ctx.request.body.operation == -1) {
+            if(userIndex == -1 || moveSeatInfo.secondary.length < 2) ctx.throw(400);
+            moveSeatInfo.secondary.splice(userIndex, 1);
+        } else if(ctx.request.body.operation == 1) {
+            if(userIndex == -1) {
+                const classInfo = await ctx.state.collection.classes.findOne({ code: parseInt(ctx.params.code, 10) });
+                if(!classInfo || moveSeatInfo.secondary.length+2 > classInfo.moveseat.limit) ctx.throw(400);
+                const addUserInfo = await ctx.state.collection.users.findOne({ code: parseInt(ctx.request.body.userInfo, 10)});
+                if(!addUserInfo) ctx.throw(400);
+                if(((1 << (addUserInfo.grade-1)) & classInfo.moveseat.group) == 0) ctx.throw(400);
+                addUserInfo.password = undefined;
+                moveSeatInfo.secondary.push(addUserInfo);
+            }
+        } else ctx.throw(400);
+
         await ctx.state.collection.moveSeatState.updateOne({ 'primary.email': ctx.state.user.email }, { $set: { secondary: moveSeatInfo.secondary } });
         await next();
     },
     common: async (ctx, next) => {
+        if(!isNumber(ctx.params.code) || ctx.params.code <= 0) ctx.throw(400);
         if(!ctx.state.user) ctx.throw(400);
+
         ctx.state.collection.moveSeatState = ctx.state.db.collection(`move-seat-group`);
         const randomDoc = (await ctx.state.collection.moveSeatState.aggregate([{
             $sample: {
