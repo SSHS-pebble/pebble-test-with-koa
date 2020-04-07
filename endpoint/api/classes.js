e2bded0dae0950db6061e3557c850cf0b687a549
chosanglyul <chosanglyul@gmail.com>
2020-04-07 15:02:50 +0000

    Remove department in class db

--- endpoint/api/classes.js
+++ endpoint/api/classes.js
@@ -2,25 +2,25 @@ const isNumber = require('is-number');
 
 module.exports = {
     post: async (ctx, next) => {
-        if(!ctx.request.body.name || !ctx.request.body.department || !ctx.request.body.building || !ctx.request.body.individual || !ctx.request.body.group) ctx.throw(400);
+        if(!ctx.request.body.name || !ctx.request.body.building || !ctx.request.body.individual || !ctx.request.body.group) ctx.throw(400);
         if(!isNumber(ctx.request.body.limit) || !isNumber(ctx.request.body.floor)) ctx.throw(400);
         if(ctx.request.body.limit <= 0 || ctx.request.body.floor <= 0) ctx.throw(400);
+        
         const individualArray = JSON.parse(ctx.request.body.individual);
         const groupArray = JSON.parse(ctx.request.body.group);
         if(!Array.isArray(individualArray) || !Array.isArray(groupArray)) ctx.throw(400);
         individualArray.forEach(allow => { if(allow < 0 || allow > 1) ctx.throw(400); })
         groupArray.forEach(allow => { if(allow < 0 || allow > 1) ctx.throw(400); })
 
-        const classes = await ctx.state.collection.classes.find().toArray()
+        const classes = await ctx.state.collection.classes.find().toArray();
         var classCode = undefined;
         if(classes.length == 0) classCode = 1;
         else classCode = classes.map(classInfo => classInfo.code).sort((a, b) => b - a)[0]+1;
         if(!isNumber(classCode)) ctx.throw(500);
-        
+
         await ctx.state.collection.classes.findOneAndUpdate({ code: classCode }, {
             $setOnInsert: {
                 name: ctx.request.body.name,
-                department: ctx.request.body.department,
                 building: ctx.request.body.building,
                 floor: parseInt(ctx.request.body.floor, 10),
                 moveseat: {
